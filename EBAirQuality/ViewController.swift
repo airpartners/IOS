4a7b75a2e9846a313901d6255759bffbb8f65d38
Nina Tchirkova <nina.tchirkova@students.olin.edu>
2020-06-08 15:55:38 -0400

    Initial Commit

---
+++ EBAirQuality/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EBAirQuality
+//
+//  Created by Nina Tchirkova on 6/8/20.
+//  Copyright © 2020 Air Partners. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
