540a9bb3ffb96f617844b5f8f65f2c524ca34ef9
Nina Tchirkova <nina.tchirkova@students.olin.edu>
2020-06-10 14:42:19 -0400

    web app shown in app

--- EBAirQuality/ViewController.swift
+++ EBAirQuality/ViewController.swift
@@ -7,14 +7,27 @@
 //
 
 import UIKit
+import WebKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, WKNavigationDelegate {
+    
+    var webView: WKWebView!
+    
+    override func loadView() {
+        webView = WKWebView()
+        webView.navigationDelegate = self
+        view = webView
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        let myURL = URL(string: "https://minhkhang1795.github.io/aq-web-client/")!
+        webView.load(URLRequest(url: myURL))
     }
 
-
+    
+    
 }
 
